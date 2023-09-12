544bd089133b54a9f509edf344fec6d9ba0652d6
Amar Gill <amardeep@clearbanc.com>
2023-09-11 22:25:29 -0700

    use fetch api for http module

--- src/http.ts
+++ src/http.ts
@@ -1,57 +1,80 @@
-import http from "https"
-
 export type HttpResponse = {
     statusCode?: number
     response: string
 }
 
-export function httpRequest(
+export async function httpRequest(
     authUrlOrigin: URL,
     apiKey: string,
     path: string,
     method: string,
     body?: string
 ): Promise<HttpResponse> {
-    let headers: any = {
+    let headers: HeadersInit = {
         Authorization: "Bearer " + apiKey,
         "Content-Type": "application/json",
     }
     if (body !== undefined) {
-        headers["Content-Length"] = Buffer.byteLength(body)
+        headers["Content-Length"] = Buffer.byteLength(body).toString()
     }
 
-    return new Promise((resolve, reject) => {
-        const req = http.request(
-            {
-                protocol: authUrlOrigin.protocol,
-                hostname: authUrlOrigin.hostname,
-                port: authUrlOrigin.port,
-                path: path,
-                method: method,
-                headers: headers,
-            },
-            (res) => {
-                let response = ""
-                res.on("data", (data) => {
-                    response += data
-                })
-                res.on("end", () => {
-                    resolve({
-                        statusCode: res.statusCode,
-                        response: response,
-                    })
-                })
-            }
-        )
+    const res = await fetch(authUrlOrigin.toString() + path, { body, method, headers })
 
-        req.on("error", (error) => {
-            reject(error)
-        })
+    const data = await res.json()
 
-        if (body !== undefined) {
-            req.write(body)
-        }
-
-        req.end()
-    })
+    return {
+        statusCode: res.status,
+        response: data,
+    }
 }
+
+// export function httpRequest(
+//     authUrlOrigin: URL,
+//     apiKey: string,
+//     path: string,
+//     method: string,
+//     body?: string
+// ): Promise<HttpResponse> {
+//     let headers: any = {
+//         Authorization: "Bearer " + apiKey,
+//         "Content-Type": "application/json",
+//     }
+//     if (body !== undefined) {
+//         headers["Content-Length"] = Buffer.byteLength(body)
+//     }
+//
+//     return new Promise((resolve, reject) => {
+//         const req = http.request(
+//             {
+//                 protocol: authUrlOrigin.protocol,
+//                 hostname: authUrlOrigin.hostname,
+//                 port: authUrlOrigin.port,
+//                 path: path,
+//                 method: method,
+//                 headers: headers,
+//             },
+//             (res) => {
+//                 let response = ""
+//                 res.on("data", (data) => {
+//                     response += data
+//                 })
+//                 res.on("end", () => {
+//                     resolve({
+//                         statusCode: res.statusCode,
+//                         response: response,
+//                     })
+//                 })
+//             }
+//         )
+//
+//         req.on("error", (error) => {
+//             reject(error)
+//         })
+//
+//         if (body !== undefined) {
+//             req.write(body)
+//         }
+//
+//         req.end()
+//     })
+// }
